2a5af523dd6c0a001ddc438567deae77b0c24758
laiozago <laiozago@gmail.com>
2023-01-07 22:07:53 -0300

    :sparkles: fiz a aba contato

--- js/main.js
+++ js/main.js
@@ -10,16 +10,34 @@ const conteudo = {
     `,
     'Curriculo': `
     <object class="object_curriculo" data="./assets/Currículo.pdf" type="application/pdf">
-    <p>Seu navegador não suporta o elemento <code>object</code>. Clique aqui para <a download="./assets/Currículo.pdf">baixar o arquivo PDF</a>.</p>
-  </object>
-  
+      <p>Seu navegador não suporta o elemento <code>object</code>. Clique aqui para <a download href="./assets/Currículo.pdf">baixar o arquivo PDF</a>.</p>
+    </object>
     `,
     'Projetos': 'Conteúdo dos projetos aqui',
-    'Contato': 'Conteúdo do contato aqui',
+    'Contato': `
+    <div class="rodape-info">
+    <div class="info-contatos">
+        <h2 class="info-contato--titulo">Contato</h2>
+        <div class="info-contato--links">
+            <a href="https://api.whatsapp.com/send?phone=5521999036284" aria-label="Link para o whatsapp" target="_blank"><i class="fa-brands fa-2xl fa-square-whatsapp"></i></a>
+            <a href="mailto:laiozago@gmail.com?subject=Portifólio" aria-label="Link para me mandar email" target="_blank"><i class="fa-solid fa-2xl fa-envelope"></i></a>
+            <a href="https://discord.com/users/laiozago#3369" aria-label="link para meu discord" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-2xl fa-discord"></i></a>
+        </div>
+    </div>
+    <div class="info-redes">
+        <h2 class="info-redes--titulo">Redes Sociais</h2>
+        <div class="info-redes--links">
+            <a href="https://github.com/laiozago" aria-label="Link para o meu github" target="_blank"><i class="fa-brands fa-2xl fa-github"></i></a>
+            <a href="https://twitter.com/laiozago" aria-label="Link para o meu twitter" target="_blank"><i class="fa-brands fa-2xl fa-square-twitter"></i></a>
+            <a href="https://www.linkedin.com/in/laiozago/" aria-label="Link para o meu linkedin" target="_blank"><i class="fa-brands fa-2xl fa-linkedin"></i></a>
+        </div>
+    </div>
+</div>
+    `,
 };
 
 display.innerHTML = conteudo['Perfil']; // Carrega o conteúdo do perfil para a div de display
-alert("Esta página ainda está em desenvolvimento"); // Exibe um alerta com a mensagem "Esta página ainda está em desenvolvimento"
+// alert("Esta página ainda está em desenvolvimento"); // Exibe um alerta com a mensagem "Esta página ainda está em desenvolvimento"
 menuLista.addEventListener('click', event => { // Adiciona um evento de clique na lista de menu
   tituloTexto.textContent = event.target.textContent; // Atualiza o texto do título da página para o texto do li clicado
   display.innerHTML = conteudo[event.target.textContent]; // Atualiza o conteúdo da div de display para o conteúdo correspondente ao li clicado
