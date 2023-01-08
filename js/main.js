a2e3860fc04664cb13fdcfe172cbdde4b72a6fa1
laiozago <laiozago@gmail.com>
2023-01-07 21:22:25 -0300

    :sparkles: alerta de página de em desenvolvimento adicionei o curriculo ao projeto

--- js/main.js
+++ js/main.js
@@ -3,14 +3,23 @@ const tituloTexto = document.querySelector('#titulo-texto'); // Seleciona o tít
 const display = document.querySelector('.display'); // Seleciona a div de display
 
 const conteudo = {
-    'Perfil': '<p>Conteúdo do perfil aqui</p><img height=200px src="assets/img/perfil.webp" alt="Foto">',
-    'Curriculo': '<iframe style="width:100%; height:100%" src="/media/laiozago/BACKUP-LAIO/backup 31.12.2022/laiozago.com.br/Equacao_do_segundo_grau/index.html"></iframe>',
+    'Perfil': `
+    <h1 class="apresentacao-info--nome">Laio Zago</h1>
+    <p class="apresentacao-info--texto">Sou professor de matemática com turmas de ensino fundamental e médio. Nessa jornada em sala de aula aprendo diariamente, sobre mim mesmo, sobre liderança, organização, como lidar com pessoas e muito mais.</p>
+    <p class="apresentacao-info--texto">Atualmente faço curso de Desenvolcedor FullStack na GrowDev e busco oportunidade para contribuir e aplicar os novos conhecimentos.</p>
+    `,
+    'Curriculo': `
+    <object class="object_curriculo" data="./assets/Currículo.pdf" type="application/pdf">
+    <p>Seu navegador não suporta o elemento <code>object</code>. Clique aqui para <a download="./assets/Currículo.pdf">baixar o arquivo PDF</a>.</p>
+  </object>
+  
+    `,
     'Projetos': 'Conteúdo dos projetos aqui',
     'Contato': 'Conteúdo do contato aqui',
 };
 
 display.innerHTML = conteudo['Perfil']; // Carrega o conteúdo do perfil para a div de display
-
+alert("Esta página ainda está em desenvolvimento"); // Exibe um alerta com a mensagem "Esta página ainda está em desenvolvimento"
 menuLista.addEventListener('click', event => { // Adiciona um evento de clique na lista de menu
   tituloTexto.textContent = event.target.textContent; // Atualiza o texto do título da página para o texto do li clicado
   display.innerHTML = conteudo[event.target.textContent]; // Atualiza o conteúdo da div de display para o conteúdo correspondente ao li clicado
